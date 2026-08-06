ae9991f0c79f9381459e384a35a9427e7aa57553
greeenlaser <sanderveski@gmail.com>
2026-08-06 13:40:42 +0300

    updated export pipeline

--- mf_ex.sh
+++ mf_ex.sh
@@ -48,6 +48,11 @@ DIR_ES=../external-shared
 IN_KH=${DIR_ES}/KalaHeaders
 OUT_KH_NAME=kalaheaders
 
+# Lua
+
+IN_LUA=${DIR_ES}/lua
+OUT_LUA_NAME=lua
+
 #
 # Core stuff
 #
@@ -80,3 +85,24 @@ for file in "${IN_KH}/include"/*; do
     name="${file##*/}"
     mf --f "$file" --t "${OUT_DIR}/${OUT_KH_NAME}/$name"
 done
+
+# Lua
+mkdir "${OUT_DIR}/${OUT_LUA_NAME}"
+
+mf --f "${IN_LUA}/${README}" --t "${OUT_DIR}/${OUT_LUA_NAME}/${README}"
+mf --f "${IN_LUA}/${LICENSE}" --t "${OUT_DIR}/${OUT_LUA_NAME}/${LICENSE}"
+
+mf --f "${IN_LUA}/include" --t "${OUT_DIR}/${OUT_LUA_NAME}"
+
+# Lua binary
+case "$1" in
+    --linux)
+        mf --f "${IN_LUA}/release/liblua.a" --t "${OUT_DIR}/${OUT_LUA_NAME}/liblua.a"
+        ;;
+    --win-on-linux)
+        mf --f "${IN_LUA}/release/lua-gnu.lib" --t "${OUT_DIR}/${OUT_LUA_NAME}/lua-gnu.lib"
+        ;;
+    --windows)
+        mf --f "${IN_LUA}/release/lua.lib" --t "${OUT_DIR}/${OUT_LUA_NAME}/lua.lib"
+        ;;
+esac
